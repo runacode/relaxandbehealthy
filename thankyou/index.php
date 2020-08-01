72a6b41cb84bdad9b20f4fc21b6dd1c0ee4c1d4e
Ethan <ethan@ewuddy.com>
2020-08-01 14:41:00 -0500

    Revert "Auto move"
    This reverts commit fa7e86cb05586ccf112e227da36dc6fe31aba609.

--- thankyou/index.php
+++ thankyou/index.php
@@ -1,22 +1,5 @@
 <?php
-<?php
-$leadid = $_REQUEST['leadid'];
-$affid  = $_REQUEST['affid'];
-$oid    = $_REQUEST['oid'];
-$campid = $_REQUEST['campid'];
-$cid    = $_REQUEST['cid'];
-$tid    = $_REQUEST['tid'];
-$udid   = $_REQUEST['udid'];
-$price  = $_REQUEST['price'];
-$s1     = isset($_REQUEST['s1'])?  $_REQUEST['s1'] : $_REQUEST['sub1'];
-$s2     = isset($_REQUEST['s2'])?  $_REQUEST['s2'] : $_REQUEST['sub2'];
-$s3     = isset($_REQUEST['s3'])?  $_REQUEST['s3'] : $_REQUEST['sub3'];
-$s4     = isset($_REQUEST['s4'])?  $_REQUEST['s4'] : $_REQUEST['sub4'];
-$s5     = isset($_REQUEST['s5'])?  $_REQUEST['s5'] : $_REQUEST['sub5'];
-
-
-
-$qs = "leadid={$leadid}&affid={$affid}&oid={$oid}&campid={$campid}&cid={$cid}&tid={$tid}&s1={$s1}&s2={$s2}&s3={$s3}&s4={$s4}&s5={$s5}&price={$price}&udid={$udid}";
+$qs = "leadid={$_REQUEST['leadid']}&affid={$_REQUEST['affid']}&oid={$_REQUEST['oid']}&campid={$_REQUEST['campid']}&cid={$_REQUEST['cid']}&tid={$_REQUEST['tid']}&s1={$_REQUEST['s1']}&s2={$_REQUEST['s2']}&s3={$_REQUEST['s3']}&s4={$_REQUEST['s4']}&s5={$_REQUEST['s5']}&price={$_REQUEST['price']}&udid={$_REQUEST['udid']}";
 
 header("location: {$_REQUEST['s1']}/thankyou/redirect.php?$qs")
 ?>
